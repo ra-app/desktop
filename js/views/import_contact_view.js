7d179664ba97abe61d5148c64ec6fa40274b7349
j.gonzalez <j.gonzalez@devcups.com>
2019-10-04 10:55:32 +0100

    add data

--- js/views/import_contact_view.js
+++ js/views/import_contact_view.js
@@ -24,7 +24,13 @@
       this.render();
       if (options){
         const parser = new DOMParser();
-        const xmlRes = parser.parseFromString(options.contact_data, 'text/xml');
+        let  xmlRes;
+        try {
+          // WebKit returns null on unsupported types
+          xmlRes = parser.parseFromString(options.contact_data, 'text/xml');
+        } catch (ex) {
+          console.log(ex)
+        }
         document.xmlRes = xmlRes;
         const contactListXml = xmlRes.children[0];
         this.createTable(contactListXml)
@@ -32,7 +38,7 @@
     },
     createTable (contactListXml){
       const table = document.createElement('table');
-      const headerTexts = ["name", "surname", "position", "email", "kunde/admin", "invitation status", "profile"];
+      const headerTexts = ['name', 'surname', 'position', 'email', 'kunde/admin', 'invitation status', 'profile'];
       if(table){
         const header = table.createTHead();
         const row = header.insertRow();
@@ -43,7 +49,22 @@
       }
       const list =  contactListXml.children;
       for (let i = 0; i < contactListXml.children.length; i++) {
-        console.log(contactListXml.children.item(i), "77777777777777")
+        const contact = contactListXml.children.item(i);
+        console.log(contact, '77777777777777')
+        const tableRow = document.createElement('tr');
+        table.appendChild(tableRow);
+        for (let j = 0; j < headerTexts.length; j++) {
+          const cellTd = document.createElement('td');
+          if(contact.getElementsByTagName(headerTexts[j])[0]){
+            const cell = contact.getElementsByTagName(headerTexts[j])[0].textContent;
+            // const cellTd = document.createElement('td');
+            const cellTdContent = document.createTextNode(cell);
+            // cellTd.style.cssText = 'text-align: left;padding: 5px;';
+            cellTd.appendChild(cellTdContent);
+          }
+          tableRow.appendChild(cellTd);
+
+        }
       }
       //Append content
       this.$('#contactTable').append(table);
