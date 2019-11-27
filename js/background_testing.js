7ff10bd638c56d44e2595cd9ee510b38d08e78de
j.gonzalez <j.gonzalez@devcups.com>
2019-11-27 12:12:31 +0000

    remove condition

--- js/background_testing.js
+++ js/background_testing.js
@@ -1504,9 +1504,9 @@ async function handleOfficeJSONMsg(envelope, message) {
   switch(msgData.type) {
     case 'note':
       console.log('handleOfficeMsgEvent note', message);
-      if(message == 'true'){
+      // if(message == 'true'){
         document.dispatchEvent(new CustomEvent('notificationNote'));
-      }
+      // }
       // await sendOfficeJsonMessage(source, { type: 'pong', msg: msgData.msg });
       break;
     // case 'pong':
