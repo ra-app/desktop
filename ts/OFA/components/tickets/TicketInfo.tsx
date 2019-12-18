9d07914700ce7fce4a8abaac8be8a11792e774ea
Carlos <c.santos@devcups.com>
2019-12-18 10:11:23 +0000

    work in progress

--- ts/OFA/components/tickets/TicketInfo.tsx
+++ ts/OFA/components/tickets/TicketInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Ticket } from '../../store/tickets/types';
+// declare var getAvatar: any;
 
 interface Props {
   ticket: Ticket;
@@ -7,6 +8,12 @@ interface Props {
 
 interface State {
   showMoreInfo: boolean;
+}
+
+declare global {
+  interface Window {
+    getpreferredLocale: any;
+  }
 }
 
 // tslint:disable-next-line:no-default-export
@@ -18,36 +25,51 @@ export default class TicketInfo extends React.Component<Props, State> {
     };
   }
 
+  public setDate(date: string) {
+    const language = window.getpreferredLocale();
+    const d = new Date(date);
+    const  options = { weekday: 'long', year: 'numeric', month: 'short',
+    day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+
+    return d.toLocaleDateString(language, options);
+  }
+
+  // if (id !== null || id !== undefined) {
+  //   arrayList[index].company_avatar = getAvatar(id);
+  // }
+  // arrayList[index].avatarSrc = getAvatar(element.client_uuid);
+
 
   public render() {
     console.log('TICKET PROPSSSS', this.props);
     const {
       ticket: {
         uuid,
-        profile_picture,
+        client_uuid,
+        name,
         surname,
-        // ts_created,
+        // profile_picture,
+        ts_created,
         state,
       }
     } = this.props;
     // const { showMoreInfo } = this.state;
-
     return (
       <div className="main-ticket-container">
         <div className="container-ticket-userinfo">
-          <img src={profile_picture} id={`clientTicketAvatar_${uuid}`} alt={`clientTicketAvatar_${uuid}`} className="ticket-user-image" />
+          {/* <img src='images/header-chat.png' id={`clientTicketAvatar_${uuid}`} alt={`clientTicketAvatar_${uuid}`} className="ticket-user-image" /> */}
           {name ? (
             <span className="ticket-user-name">{name} {surname}</span>
           ) : (
-            <span className="ticket-user-name">username</span>
-          )}
+              <span className="ticket-user-name">username</span>
+            )}
         </div>
         <div className="container-ticket-info">
-          <span className="ticket-id">Ticket {uuid}</span>
-          {/* <span className="ticket-date">${
-                  days[new Date(mssg.ts).getDay()]
-                  } ${new Date(mssg.ts).getHours() -
-                  1}:${new Date(mssg.ts).getMinutes()}</span> */}
+          <span className="ticket-id">Ticket </span>
+          {/* <span className="ticket-id">Ticket {uuid}</span> */}
+          <span className="ticket-date">
+            {this.setDate(ts_created)}
+          </span>
         </div>
 
         <div className="container-ticket-actions">
@@ -58,9 +80,9 @@ export default class TicketInfo extends React.Component<Props, State> {
           {state === 2 ? (
             <button id={`claim_${uuid}`} className="button-claim-ticket claimed" disabled={true}>Übernommen</button>
           ) : (
-            // can be unknown, unclaim or close
-            <button id={`claim_${uuid}`} className="button-claim-ticket not-claimed">Übernehmen</button>
-          )}
+              // can be unknown, unclaim or close
+              <button id={`claim_${uuid}`} className="button-claim-ticket not-claimed">Übernehmen</button>
+            )}
         </div>
         {/* {showMoreInfo} */}
       </div>
