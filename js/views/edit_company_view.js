2d522cc3e2f5f0fd76249b3db29b633218aa03ab
Carlos <c.santos@devcups.com>
2019-12-26 11:26:26 +0000

    edit company - work in progress

--- js/views/edit_company_view.js
+++ js/views/edit_company_view.js
@@ -29,119 +29,119 @@
     },
 
     events: {
-      'click #editName': 'editCompanyName',
-      'click #buttonSaveEditCompany': 'saveNewCompanyName',
-      'click #editAvatar': 'editCompanyAvatar',
-      'change #inputNewAvatar': 'onChooseAvatarFile',
-      'click #closeEdit': 'closeEdit',
+      // 'click #editName': 'editCompanyName',
+      // 'click #buttonSaveEditCompany': 'saveNewCompanyName',
+      // 'click #editAvatar': 'editCompanyAvatar',
+      // 'change #inputNewAvatar': 'onChooseAvatarFile',
+      // 'click #closeEdit': 'closeEdit',
     },
-    editCompanyAvatar() {
-      this.$('#inputNewAvatar').click();
-    },
-    closeEdit() {
-      if (this.$('#edit-company-container')) {
-        document.getElementById('edit-company-container').remove();
-      }
-    },
-    async onChooseAvatarFile() {
-      const company_id = textsecure.storage.get('companyNumber', null)
-      const fileField = this.$('#inputNewAvatar')[0].files[0];
-      let base64 = '';
-      const imageType = 'image/png' // this.$('#inputNewAvatar')[0].files[0].type;
-      const width = 140;
-      const height = 140;
-      const fileName = this.$('#inputNewAvatar')[0].files[0].name;
-      const reader = new FileReader();
-      reader.readAsDataURL(fileField);
-      reader.onload = event => {
-        const img = new Image();
-        img.src = event.target.result;
-        // eslint-disable-next-line no-unused-expressions
-        (img.onload = () => {
-          const elem = document.createElement('canvas');
-           elem.width = width;
-           elem.height = height;
-          const ctx = elem.getContext('2d');
+    // editCompanyAvatar() {
+    //   this.$('#inputNewAvatar').click();
+    // },
+    // closeEdit() {
+    //   if (this.$('#edit-company-container')) {
+    //     document.getElementById('edit-company-container').remove();
+    //   }
+    // },
+    // async onChooseAvatarFile() {
+    //   const company_id = textsecure.storage.get('companyNumber', null)
+    //   const fileField = this.$('#inputNewAvatar')[0].files[0];
+    //   let base64 = '';
+    //   const imageType = 'image/png' // this.$('#inputNewAvatar')[0].files[0].type;
+    //   const width = 140;
+    //   const height = 140;
+    //   const fileName = this.$('#inputNewAvatar')[0].files[0].name;
+    //   const reader = new FileReader();
+    //   reader.readAsDataURL(fileField);
+    //   reader.onload = event => {
+    //     const img = new Image();
+    //     img.src = event.target.result;
+    //     // eslint-disable-next-line no-unused-expressions
+    //     (img.onload = () => {
+    //       const elem = document.createElement('canvas');
+    //        elem.width = width;
+    //        elem.height = height;
+    //       const ctx = elem.getContext('2d');
 
-          // img.width and img.height will contain the original dimensions
-          ctx.drawImage(img, ((img.naturalWidth/2)-(300/2)), ((img.naturalHeight/2)-(300/2)), 300, 300, 0, 0,width, height);
-          ctx.canvas.toBlob(
-            async blob => {
-              base64 = new File([blob], fileName, {
-                type: imageType,
-                lastModified: Date.now(),
-              });
-              base64 = await toBase64(base64);
-              if (base64) {
-                this.$('#companyAvatar').attr('src', base64);
-              }
-              // eslint-disable-next-line prefer-destructuring
-              base64 = base64.split(',')[1];
-              const dataCompanyAvatar = {
-                data: base64,
-                type: imageType.split('/')[1],
-              };
-              textsecure.storage.put('dataCompanyAvatar', dataCompanyAvatar);
-              this.newAvatar = true;
-              this.$('#buttonSaveEditCompany').removeClass('disabled')
-              // setCompanyAvatar(company_id, dataCompanyAvatar);
-            }, imageType, 1
-          );
-          // eslint-disable-next-line no-sequences
-        }),
-          (reader.onerror = error => console.log(error));
-      };
-    },
-    editCompanyName() {
-      this.$('#nameSection').hide();
-      const settingEditName = document.createElement('div');
-      settingEditName.id = 'settingEditName';
+    //       // img.width and img.height will contain the original dimensions
+    //       ctx.drawImage(img, ((img.naturalWidth/2)-(300/2)), ((img.naturalHeight/2)-(300/2)), 300, 300, 0, 0,width, height);
+    //       ctx.canvas.toBlob(
+    //         async blob => {
+    //           base64 = new File([blob], fileName, {
+    //             type: imageType,
+    //             lastModified: Date.now(),
+    //           });
+    //           base64 = await toBase64(base64);
+    //           if (base64) {
+    //             this.$('#companyAvatar').attr('src', base64);
+    //           }
+    //           // eslint-disable-next-line prefer-destructuring
+    //           base64 = base64.split(',')[1];
+    //           const dataCompanyAvatar = {
+    //             data: base64,
+    //             type: imageType.split('/')[1],
+    //           };
+    //           textsecure.storage.put('dataCompanyAvatar', dataCompanyAvatar);
+    //           this.newAvatar = true;
+    //           this.$('#buttonSaveEditCompany').removeClass('disabled')
+    //           // setCompanyAvatar(company_id, dataCompanyAvatar);
+    //         }, imageType, 1
+    //       );
+    //       // eslint-disable-next-line no-sequences
+    //     }),
+    //       (reader.onerror = error => console.log(error));
+    //   };
+    // },
+    // editCompanyName() {
+    //   this.$('#nameSection').hide();
+    //   const settingEditName = document.createElement('div');
+    //   settingEditName.id = 'settingEditName';
 
-      const textarea = document.createElement('textarea');
-      textarea.className = 'editTextareaCompanyName'
-      textarea.id = 'newCompanyName';
-      textarea.placeholder = 'new company name';
-      // textarea.innerHTML = this.model[0].company_name;
-      textarea.innerHTML = this.$('#spanCompanyName').text();
-      // const doneIcon = document.createElement('img');
-      // doneIcon.id = 'doneIcon';
-      // doneIcon.src = 'images/icons/edit-blue.svg'
-      // doneIcon.className = 'editIcon'
-      textarea.onkeyup = (evt) => {
-        const nameCompany = evt.target.value
-        console.log(evt.target.value.length)
-        if(nameCompany.length > 0){
-          this.$('#buttonSaveEditCompany').removeClass('disabled')
-        }else {
-          this.$('#buttonSaveEditCompany').addClass('disabled')
-        }
-      }
+    //   const textarea = document.createElement('textarea');
+    //   textarea.className = 'editTextareaCompanyName'
+    //   textarea.id = 'newCompanyName';
+    //   textarea.placeholder = 'new company name';
+    //   // textarea.innerHTML = this.model[0].company_name;
+    //   textarea.innerHTML = this.$('#spanCompanyName').text();
+    //   // const doneIcon = document.createElement('img');
+    //   // doneIcon.id = 'doneIcon';
+    //   // doneIcon.src = 'images/icons/edit-blue.svg'
+    //   // doneIcon.className = 'editIcon'
+    //   textarea.onkeyup = (evt) => {
+    //     const nameCompany = evt.target.value
+    //     console.log(evt.target.value.length)
+    //     if(nameCompany.length > 0){
+    //       this.$('#buttonSaveEditCompany').removeClass('disabled')
+    //     }else {
+    //       this.$('#buttonSaveEditCompany').addClass('disabled')
+    //     }
+    //   }
 
-      settingEditName.append(textarea);
-      // settingEditName.append(doneIcon);
-      this.$('#buttonSaveEditCompany').removeClass('disabled')
-      this.$('#edit_company_data').append(settingEditName)
-    },
-    async saveNewCompanyName() {
-      this.$('#buttonSaveEditCompany').addClass('disabled')
-      const company_id = textsecure.storage.get('companyNumber', null);
-      const newName = this.$('#newCompanyName').val();
-      if(this.newAvatar) {
-        const dataCompanyAvatar = textsecure.storage.get('dataCompanyAvatar');
-        setCompanyAvatar(company_id, dataCompanyAvatar);
-      }
-      if(newName !== undefined){
-        await updateCompanyName(newName, company_id)
-      }
-      await getCompany(company_id);
-      this.$('#spanCompanyName').text(newName);
-      this.$('#settingEditName').remove();
-      this.$('#nameSection').show();
-      window.clearCache();
-      if(this.newAvatar) {
-        updateImagesByUrl(CENTRAL_IMG_ROOT + company_id);
-      }
-      // this.render();
-    },
+    //   settingEditName.append(textarea);
+    //   // settingEditName.append(doneIcon);
+    //   this.$('#buttonSaveEditCompany').removeClass('disabled')
+    //   this.$('#edit_company_data').append(settingEditName)
+    // },
+    // async saveNewCompanyName() {
+    //   this.$('#buttonSaveEditCompany').addClass('disabled')
+    //   const company_id = textsecure.storage.get('companyNumber', null);
+    //   const newName = this.$('#newCompanyName').val();
+    //   if(this.newAvatar) {
+    //     const dataCompanyAvatar = textsecure.storage.get('dataCompanyAvatar');
+    //     setCompanyAvatar(company_id, dataCompanyAvatar);
+    //   }
+    //   if(newName !== undefined){
+    //     await updateCompanyName(newName, company_id)
+    //   }
+    //   await getCompany(company_id);
+    //   this.$('#spanCompanyName').text(newName);
+    //   this.$('#settingEditName').remove();
+    //   this.$('#nameSection').show();
+    //   window.clearCache();
+    //   if(this.newAvatar) {
+    //     updateImagesByUrl(CENTRAL_IMG_ROOT + company_id);
+    //   }
+    //   // this.render();
+    // },
   });
 })();
--- ts/OFA/components/editCompany/EditCompany.tsx
+++ ts/OFA/components/editCompany/EditCompany.tsx
@@ -1,25 +1,113 @@
 import React, { Fragment } from 'react';
 import Avatar from '../avatar/Avatar';
 import { CompanyInfo } from '../../store/companyInfo/types';
-
+declare var updateCompanyName: any;
+declare var setCompanyAvatar: any;
+declare var updateImagesByUrl: any;
+declare var CENTRAL_IMG_ROOT: string;
+declare var toBase64: any;
 interface Props {
   info: CompanyInfo;
 }
 
-// interface State {
-
-// }
-
-// export default class EditCompany extends React.Component<Props, State> {
+interface State {
+  editingCompanyName: boolean;
+  companyNameValue: string;
+  companyAvatarValue: any;
+  disabledSaveCompany: boolean;
+}
 // tslint:disable-next-line:no-default-export
-export default class EditCompany extends React.Component<Props> {
+export default class EditCompany extends React.Component<Props, State> {
+  // newAvatar: boolean;
   constructor(props: Props) {
     super(props);
-    // this.state = {
-    // };
+    this.state = {
+      editingCompanyName: false,
+      companyNameValue: this.props.info.name,
+      companyAvatarValue: { data: null, type: null },
+      disabledSaveCompany: true,
+    };
   }
 
-  // public componentDidMount() {}
+  public editCompanyName() {
+    this.setState({ editingCompanyName: !this.state.editingCompanyName });
+  }
+
+  public updatingCompanyName(e: any) {
+    if (this.state.disabledSaveCompany) {
+      this.setState({ disabledSaveCompany: false });
+    }
+    if (e.target.value === '' || e.target.value === ' ') {
+      this.setState({ disabledSaveCompany: true });
+    }
+    this.setState({ companyNameValue: e.target.value });
+  }
+
+  public openFileSelector() {
+    if (document.getElementById('inputNewAvatar')) {
+      // tslint:disable-next-line:no-non-null-assertion
+      document.getElementById('inputNewAvatar')!.click();
+    }
+  }
+
+  public updatingCompanyAvatar(e: any) {
+    const currentFile = e.target.files[0];
+    let base64: any = '';
+    const imageType = 'image/png';
+    const width = 140;
+    const height = 140;
+    const fileName = currentFile.name;
+    const reader = new FileReader();
+    reader.readAsDataURL(currentFile);
+    reader.onload = () => {
+      const img: any = new Image();
+      img.src = reader.result;
+      // tslint:disable-next-line:ban-comma-operator
+      (img.onload = () => {
+        const elem = document.createElement('canvas');
+        elem.width = width;
+        elem.height = height;
+        const ctx: any = elem.getContext('2d');
+        ctx.drawImage(img, ((img.naturalWidth / 2) - (300 / 2)), ((img.naturalHeight / 2) - (300 / 2)), 300, 300, 0, 0, width, height);
+        ctx.canvas.toBlob(
+          async (blob: BlobPart) => {
+            base64 = new File([blob], fileName, {
+              type: imageType,
+              lastModified: Date.now(),
+            });
+            base64 = await toBase64(base64);
+            base64 = base64.split(',')[1];
+            const dataCompanyAvatar = {
+              data: base64,
+              type: imageType.split('/')[1],
+            };
+            this.setState({ disabledSaveCompany: false, companyAvatarValue: dataCompanyAvatar });
+          }, imageType, 1
+        );
+      }),
+        (reader.onerror = error => console.log(error));
+    };
+  }
+
+  public async updateCompanyInfo() {
+    // double check before to upload the compan
+    const companyID = this.props.info.company_number;
+    const companyName = this.props.info.name;
+    if (!this.state.disabledSaveCompany) {
+      if (this.state.companyNameValue !== companyName) {
+        await updateCompanyName(this.state.companyNameValue, companyID);
+      }
+      if (this.state.companyAvatarValue.data !== null) {
+        setCompanyAvatar(companyID, this.state.companyAvatarValue);
+        updateImagesByUrl(CENTRAL_IMG_ROOT + companyID);
+      }
+      this.setState({ disabledSaveCompany: true });
+    }
+  }
+
+  public closeEdit() {
+    console.log('CLOSEEE');
+  }
 
   public render() {
     const {
@@ -31,36 +119,48 @@ export default class EditCompany extends React.Component<Props> {
     } = this.props;
 
     return (
-     <Fragment>
-      <div id="edit_company_data" className="edit-company-container">
-        <div className="companyAvatarContainer">
-          <div className="avatarCompanyContainer">
-            <Avatar avatarSrc={company_avatar} id={`companyAvatar-${company_number}`}/>
-          </div>
-          <div className="buttonEditCompany">
-            <div>
-              <img className="closeIcon" id="closeEdit" src="images/icons/x-searchbar.svg" alt="close edit company" />
+      <Fragment>
+        <div id="edit_company_data" className="edit-company-container">
+          <div className="companyAvatarContainer">
+            <div className="avatarCompanyContainer">
+              <Avatar avatarSrc={company_avatar} id={`companyAvatar-${company_number}`} />
             </div>
-            <div className="changeAvatar">
-              <input type="file" className="hidden" id="inputNewAvatar" placeholder="input file" />
-              <img className="editIconAvatar" id="editAvatar" src="images/icons/edit-blue.svg" alt="upload new avatar"/>
+            <div className="buttonEditCompany">
+              <div>
+                <img onClick={() => this.closeEdit()} className="closeIcon" id="closeEdit" src="images/icons/x-searchbar.svg" alt="close edit company" />
+              </div>
+              <div className="changeAvatar">
+                <input type="file" onChange={e => this.updatingCompanyAvatar(e)} className="hidden" id="inputNewAvatar" placeholder="input file" />
+                <img onClick={() => this.openFileSelector()} className="editIconAvatar" id="editAvatar" src="images/icons/edit-blue.svg" alt="upload new avatar" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="containerLabelInfoCompany">
-          <label className="labelEditName">Name des Unternehmens</label>
-        </div>
-        <div className="containerDataEditCompany" id="nameSection">
-          <div className="companyNameEdit">
-            <span id="spanCompanyName">{name}</span>
+          <div className="containerLabelInfoCompany">
+            <label className="labelEditName">Name des Unternehmens</label>
           </div>
-          <div className="buttonEditCompany">
-            <img className="editIcon" id="editName" src="images/icons/edit-blue.svg" alt="edit name company"/>
-          </div>
+          {this.state.editingCompanyName ? (
+            <input id="companyNameInput" onChange={e => this.updatingCompanyName(e)} value={this.state.companyNameValue} />
+          ) : (
+              <div className="containerDataEditCompany" id="nameSection">
+                <div className="companyNameEdit">
+                  <span id="spanCompanyName">{name}</span>
+                </div>
+                {/* tslint:disable-next-line:react-a11y-event-has-role */}
+                <div className="buttonEditCompany" onClick={() => this.editCompanyName()}>
+                  <img className="editIcon" id="editName" src="images/icons/edit-blue.svg" alt="edit name company" />
+                </div>
+              </div>
+            )}
+          <button
+            className={`buttonSaveEditCompany ${this.state.disabledSaveCompany && 'disabled'}`}
+            disabled={this.state.disabledSaveCompany}
+            id="buttonSaveEditCompany"
+            onClick={() => this.updateCompanyInfo()}
+          >
+            Speichern
+          </button>
         </div>
-        <button className="buttonSaveEditCompany disabled"  id="buttonSaveEditCompany">Speichern</button>
-      </div>
-     </Fragment>
+      </Fragment>
     );
   }
 }
