792bc16a8a8d3ac6df92edb68a7e18d432014851
j.gonzalez <j.gonzalez@devcups.com>
2019-12-26 08:36:41 +0000

    close tickets

--- js/modules/OFA.js
+++ js/modules/OFA.js
@@ -1,9 +1,11 @@
 const TicketsView = require('../../ts/OFA/components/tickets/TicketsView').default;
+const ConversationHeader = require('../../ts/components/conversation/ConversationHeader').default;
 const { wrapWithReduxStoreOnElem } = require('../../ts/OFA/util/StoreWrapper');
 const { storeInstance } = require('../../ts/OFA/store/index');
 
 module.exports = {
   TicketsView,
+  ConversationHeader,
   wrapWithReduxStoreOnElem,
   store: storeInstance,
 };
