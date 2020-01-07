8d8306feba210af0fa79469f9843889b6791fdf8
Carlos <c.santos@devcups.com>
2020-01-07 16:05:02 +0000

    fix parameter error

--- js/views/conversation_view.js
+++ js/views/conversation_view.js
@@ -484,7 +484,7 @@
             // });
             conversation.sendMessage(message);
             conversation.sendMessage(messageLine);
-            closeTicketBySignal(external_number, {id: id});
+            closeTicketBySignal(external_number, {id: external_number});
             this.model.setClosed(true);
             setTimeout(() => {
               this.unload('archive');
